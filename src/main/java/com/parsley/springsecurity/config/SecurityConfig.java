a34950b23301a2980cdfb85dbfc1aa0f31e4601b
Piotr Janczak <piotr.janczak1112@gmail.com>
2024-05-20 15:28:32 +0200

    hashing password added

--- src/main/java/com/parsley/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/parsley/springsecurity/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,16 +27,9 @@ public class SecurityConfig {
         return http.build();
     }
 
-    /**
-     * NoOpPasswordEncoder is not recommended for production usage.
-     * Use only for non-prod.
-     *
-     * @return PasswordEncoder
-     */
-
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
