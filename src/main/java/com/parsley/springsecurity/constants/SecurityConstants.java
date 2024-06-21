653d7f32e362f2c236bb27134129eeb0efc82aa9
Piotr Janczak <piotr.janczak1112@gmail.com>
2024-06-21 17:07:05 +0200

    JTW filters and SecurityConstants added

---
+++ src/main/java/com/parsley/springsecurity/constants/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.parsley.springsecurity.constants;
+
+public interface SecurityConstants {
+
+    public static final String JWT_KEY = "jxgEQesrthjTHV6tjst654GDYUn4";
+    public static final String JWT_HEADER = "Authorization";
+
+}
